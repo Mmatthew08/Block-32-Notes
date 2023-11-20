fab1bc217ad508820ffc0e644edd0267935fa9c7
Matthew Eski <metehaneski08@gmail.com>
2023-11-19 20:42:03 -0600

    notes

---
+++ notes.js
@@ -0,0 +1,6 @@
+// Restful API's: Representational,State,Transfer
+
+//API Testing Tools:
+//Postman: is a tool that can be used to make http request to an API.
+//Curl: can be used for the endpoints of an API
+// Fetch: is mostly used for development
---
+++ purchase.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const router= express.Router();
+const {getPurchases,getPurchase,createPurchase,}
